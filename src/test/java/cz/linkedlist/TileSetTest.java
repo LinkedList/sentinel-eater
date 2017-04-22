02bdb56989ade3670c2dad864b1e799d3e5606d2
Martin Macko <macko.mar@gmail.com>
2017-04-22 15:54:14 +0200

    Adds tests for new functionality of Tile downloader

--- src/test/java/cz/linkedlist/TileSetTest.java
+++ src/test/java/cz/linkedlist/TileSetTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.util.Optional;
 
+import static cz.linkedlist.TileSet.Contents.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
@@ -106,4 +107,27 @@ public class TileSetTest {
 
         assertThat(tileSet.cloudiness(), is(50D));
     }
+
+    @Test
+    public void testContentsMap() {
+        TileSet tileSet = TileSet.of("28CDD", 2017, 3, 10);
+
+        assertThat(BAND_1.map(tileSet), is(tileSet.band(1).get()));
+        assertThat(BAND_2.map(tileSet), is(tileSet.band(2).get()));
+        assertThat(BAND_3.map(tileSet), is(tileSet.band(3).get()));
+        assertThat(BAND_4.map(tileSet), is(tileSet.band(4).get()));
+        assertThat(BAND_5.map(tileSet), is(tileSet.band(5).get()));
+        assertThat(BAND_6.map(tileSet), is(tileSet.band(6).get()));
+        assertThat(BAND_7.map(tileSet), is(tileSet.band(7).get()));
+        assertThat(BAND_8.map(tileSet), is(tileSet.band(8).get()));
+        assertThat(BAND_9.map(tileSet), is(tileSet.band(9).get()));
+        assertThat(BAND_10.map(tileSet), is(tileSet.band(10).get()));
+        assertThat(BAND_11.map(tileSet), is(tileSet.band(11).get()));
+        assertThat(BAND_12.map(tileSet), is(tileSet.band(12).get()));
+        assertThat(BAND_8A.map(tileSet), is(tileSet.band8A()));
+        assertThat(PROD_INFO.map(tileSet), is(tileSet.productInfo()));
+        assertThat(TILE_INFO.map(tileSet), is(tileSet.tileInfo()));
+        assertThat(METADATA.map(tileSet), is(tileSet.metadata()));
+    }
+
 }
--- src/test/java/cz/linkedlist/http/HttpTileDownloaderTest.java
+++ src/test/java/cz/linkedlist/http/HttpTileDownloaderTest.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -82,5 +83,33 @@ public class HttpTileDownloaderTest {
         assertThat(Files.exists(path), is(true));
         assertTrue(Files.size(path) > 0);
         Files.deleteIfExists(path);
+    }
+
+    @Test
+    public void testDownContent() throws Exception {
+        TileSet tileSet = new TileSet(UTMCode.of("36MTD"), LocalDate.of(2016, 8, 31));
+        Path path = Paths.get("/tmp/" + tileSet.band(1).get().replace("/", "_"));
+        Files.deleteIfExists(path);
+        downloader.downContent(tileSet, TileSet.Contents.BAND_1);
+        assertThat(Files.exists(path), is(true));
+        assertTrue(Files.size(path) > 0);
+        Files.deleteIfExists(path);
+    }
+
+    @Test
+    public void testDownContents() throws Exception {
+        TileSet tileSet = new TileSet(UTMCode.of("36MTD"), LocalDate.of(2016, 8, 31));
+        Path path1 = Paths.get("/tmp/" + tileSet.band(1).get().replace("/", "_"));
+        Path path2 = Paths.get("/tmp/" + tileSet.productInfo().replace("/", "_"));
+        Files.deleteIfExists(path1);
+        Files.deleteIfExists(path2);
+        downloader.downContent(tileSet, Arrays.asList(TileSet.Contents.BAND_1, TileSet.Contents.PROD_INFO));
+        assertThat(Files.exists(path1), is(true));
+        assertTrue(Files.size(path1) > 0);
+        Files.deleteIfExists(path1);
+
+        assertThat(Files.exists(path2), is(true));
+        assertTrue(Files.size(path2) > 0);
+        Files.deleteIfExists(path2);
     }
 }
